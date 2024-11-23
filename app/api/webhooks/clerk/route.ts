913a715e265bd0fe8578075d23f1221fa89ba16f
vladEDT <vladischanov@gmail.com>
2024-11-24 01:26:47 +0300

    fix

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -16,9 +16,9 @@ export async function POST(req: Request) {
   }
 
   const headerPayload = headers()
-  const svix_id = headerPayload.get('svix-id')
-  const svix_timestamp = headerPayload.get('svix-timestamp')
-  const svix_signature = headerPayload.get('svix-signature')
+  const svix_id = (await headerPayload).get('svix-id')
+  const svix_timestamp = (await headerPayload).get('svix-timestamp')
+  const svix_signature = (await headerPayload).get('svix-signature')
 
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response('Error occurred -- no svix headers', {
